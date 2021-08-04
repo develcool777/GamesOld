4b4626e47316728d93c173705f47ccf3eeb697cf
Вадим Павлюк <dyletant@ukr.net>
2021-08-04 17:59:02 +0300

    Faster render gameBlocks on home page

--- src/store/modules/games.js
+++ src/store/modules/games.js
@@ -38,67 +38,8 @@ export default {
         })
       }
 
-      const IMGS = [];
-      const getImgs = async () => {
-        const storageImgs = firebase.storage().ref('gamesPreview/imgs');
-        const imgNames = (await storageImgs.listAll()).items.map(i => i.name);
-        const promises = imgNames.map(async name => {
-          const itemRef = storageImgs.child(name);
-          const imgUrl = await itemRef.getDownloadURL();
-          return {name, imgUrl};
-        })
-        const res = await Promise.all(promises);
-        res.forEach(imgUrl => IMGS.push(imgUrl))
-      }
-
-      const VIDEOS = [];
-      const getVideos = async () => {
-        const storageImgs = firebase.storage().ref('gamesPreview/videos');
-        const videoNames = (await storageImgs.listAll()).items.map(i => i.name);
-        const promises = videoNames.map(async name => {
-          const itemRef = storageImgs.child(name);
-          const videoUrl = await itemRef.getDownloadURL();
-          return {name, videoUrl};
-        })
-        const res = await Promise.all(promises);
-        res.forEach(videoUrl => VIDEOS.push(videoUrl))
-      }
-
-      const merge1 = () => {
-        DATA.forEach((data, i) => {
-          if (IMGS.length === 1) {
-            DATA[i].imgUrl = IMGS[0].imgUrl;
-            return; 
-          }
-          IMGS.forEach((img, j) => {
-            if (data.imgName === img.name) {
-              DATA[i].imgUrl = img.imgUrl;
-              IMGS.splice(j, 1);
-              return;
-            }
-          })
-        })
-      }
-      const merge2 = () => {
-        DATA.forEach((data, i) => {
-          if (VIDEOS.length === 1) {
-            DATA[i].videoUrl = VIDEOS[0].videoUrl;
-            return; 
-          }
-          VIDEOS.forEach((video, j) => {
-            if (data.videoName === video.name) {
-              DATA[i].videoUrl = video.videoUrl;
-              VIDEOS.splice(j, 1);
-              return;
-            }
-          })
-        })
-      }
       await getData();
-      await getImgs();
-      await getVideos();
-      merge1();
-      merge2();
+
       DATA.sort((a, b) => a.id - b.id);
       console.log(DATA);
       commit('setData', DATA);
