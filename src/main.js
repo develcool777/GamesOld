62af8279fd35a7efe3dd8faae814c6e7d48b94f8
Вадим Павлюк <dyletant@ukr.net>
2021-09-10 10:39:52 +0100

    Firebase - config file

--- src/main.js
+++ src/main.js
@@ -3,18 +3,10 @@ import App from './App.vue'
 import router from './router'
 import store from "@/store";
 import firebase from 'firebase/app';
+import firebaseConfig from '../firebase.json';
 
-var firebaseConfig = {
-  apiKey: "AIzaSyAN74W4lQ08kNqnVwpu6tWkvF6chy2J09o",
-  authDomain: "games-65e21.firebaseapp.com",
-  projectId: "games-65e21",
-  storageBucket: "games-65e21.appspot.com",
-  messagingSenderId: "325996537580",
-  appId: "1:325996537580:web:e037778742f6d7ce9a27c9",
-  measurementId: "G-KQ9PVDZSK4"
-};
 // Initialize Firebase
-firebase.initializeApp(firebaseConfig);
+firebase.initializeApp(firebaseConfig.config);
 
 const app = createApp(App);
 app.use(store);
