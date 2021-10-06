9df104324bcaf4a58032b3ddbbb0cf34a6566a84
Вадим Павлюк <dyletant@ukr.net>
2021-10-06 19:25:20 +0100

    FontAwesome is added

---
+++ src/fontawesome.js
@@ -0,0 +1,17 @@
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { faChessRook } from "@fortawesome/free-solid-svg-icons";
+import { faChessBishop } from "@fortawesome/free-solid-svg-icons";
+import { faChessKnight } from "@fortawesome/free-solid-svg-icons";
+import { faChessKing } from "@fortawesome/free-solid-svg-icons";
+import { faChessQueen } from "@fortawesome/free-solid-svg-icons";
+import { faChessPawn } from "@fortawesome/free-solid-svg-icons";
+
+library.add(faChessRook);
+library.add(faChessBishop);
+library.add(faChessKnight);
+library.add(faChessKing);
+library.add(faChessQueen);
+library.add(faChessPawn);
+
+export default FontAwesomeIcon;
--- src/main.js
+++ src/main.js
@@ -4,6 +4,7 @@ import router from './router'
 import store from "@/store";
 import firebase from 'firebase/app';
 import firebaseConfig from '../firebase.json';
+import FontAwesomeIcon from "@/fontawesome";
 
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig.config);
@@ -11,3 +12,4 @@ firebase.initializeApp(firebaseConfig.config);
 const app = createApp(App);
 app.use(store);
 app.use(router).mount('#app');
+app.component("font-awesome-icon", FontAwesomeIcon);
