4156ccb9396e2e7bb402e568929ebd24607c7a1a
Вадим Павлюк <dyletant@ukr.net>
2021-10-15 16:08:59 +0100

    codeQL2

--- server.js
+++ server.js
@@ -8,9 +8,9 @@ const app = express()
 app.use('/', serveStatic(path.join(__dirname, '/dist')))
 
 // this * route is to serve project on different page routes except root `/`
-app.get(/./, function (req, res) {
-	res.sendFile(path.join(__dirname, '/dist/index.html'))
-})
+// app.get(/.*/, function (req, res) {
+// 	res.sendFile(path.join(__dirname, '/dist/index.html'))
+// })
 
 const port = process.env.PORT || 8080
 app.listen(port)
