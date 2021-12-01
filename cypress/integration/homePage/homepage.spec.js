318f1e11d4fe915494b8f5d19763503b81d62f22
Вадим Павлюк <dyletant@ukr.net>
2021-12-01 09:09:51 +0000

    Cypress - second attempt

--- cypress/integration/homePage/homepage.spec.js
+++ cypress/integration/homePage/homepage.spec.js
@@ -1,6 +1,30 @@
 describe('visit Website', () => {
-  it('link', () => {
-    cy.visit('http://localhost:8080/#/');
+  it('home page', () => {
+    cy.visit('/');
   });
-  
+
+  it('Maze game', () => {
+    cy.visit('http://localhost:8080/#/maze');
+    cy.visit('http://localhost:8080/#/maze/info');
+  });
+
+  it('Memoji game', () => {
+    cy.visit('http://localhost:8080/#/memoji');
+    cy.visit('http://localhost:8080/#/memoji/info');
+  });
+
+  it('Rock Paper Scissors game', () => {
+    cy.visit('http://localhost:8080/#/rock-paper-scissors');
+    cy.visit('http://localhost:8080/#/rock-paper-scissors/info');
+  });
+
+  it('Tic Tac Toe game', () => {
+    cy.visit('http://localhost:8080/#/tic-tac-toe');
+    cy.visit('http://localhost:8080/#/tic-tac-toe/info');
+  });
+
+  it('Chess game', () => {
+    cy.visit('http://localhost:8080/#/chess');
+    cy.visit('http://localhost:8080/#/chess/info');
+  });
 });
